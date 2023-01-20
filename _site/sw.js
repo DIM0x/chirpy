ed93de1dbce78d2d9ba7987f9cf36cb1a064a803
dimo <forker4@dim0.uu.me>
2023-01-20 14:25:56 +0800

    update pinned blog

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/chirpy/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230120.134249';
+const cacheName = 'chirpy-20230120.142535';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
